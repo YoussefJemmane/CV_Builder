33be3f3639b33c1b2597144c9f9e0d7017f3884f
YoussefJemmane <JEMMANE.YOUSSEF@ofppt-edu.ma>
2023-06-05 11:49:09 +0100

    making the api

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,14 @@ app.use('/api/user', userRoutes)
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+    if (process.env.PORT) {
+      app.listen(process.env.PORT, () => {
+        console.log('connected to db & listening on port', process.env.PORT)
+      })
+    }
   })
-  .catch((error) => { 
+  .catch((error) => {
     console.log(error)
   })
+
+module.exports = app;
